19c11aa3a701bccff777cd358d9092f59bc50a17
Benedikt Aigner <benedikt.aigner@stud.fh-campuswien.ac.at>
2024-06-24 18:06:07 +0200

    added DB

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
+import { SeriesStateModule } from '../modules/series/series-state.module';
+import { SeriesEventsModule } from '../modules/websocket/series-events.module';
+import { FileDownloadModule } from '../modules/file-download/file-download.module';
+import { SeriesStateService } from '../modules/series/series-state.service';
+import { CSGOTeamsModule } from '../cs teams/csgo-teams.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import configuration from '../config/configuration';
 
 @Module({
@@ -23,10 +31,15 @@ import configuration from '../config/configuration';
       },
       inject: [ConfigService],
     }),
+    HttpModule,
+    CSGOTeamsModule,
+    SeriesStateModule,
+    SeriesEventsModule,
+    FileDownloadModule,
     UsersModule,
     AuthModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService, SeriesStateService],
 })
 export class AppModule { }
