8499b68f16bf2c0087f58246ed12d82044a96e69
Fadyheriza <fady.heriza@stud.fh-campuswien.ac.at>
2024-06-24 23:22:28 +0200

    push 2

--- backend/src/cs teams/csgo-teams.service.ts
+++ backend/src/cs teams/csgo-teams.service.ts
@@ -20,26 +20,40 @@ export class CSGOTeamsService {
     this.logger.log(`API Key: ${this.apiKey}`);
   }
 
-  async getTeam(teamId: string): Promise<any> {
+  async getTeams(): Promise<any> {
     if (!this.apiUrl || !this.apiKey) {
       this.logger.error('API URL or API Key is missing');
       throw new Error('API URL or API Key is missing');
     }
 
     const query = `
-      query GetTeam {
-        team(id: "${teamId}") {
-          id
-          name
-          colorPrimary
-          colorSecondary
-          logoUrl
-          externalLinks {
-            dataProvider {
-              name
-            }
-            externalEntity {
+      query GetTeams {
+        teams(first: 50, after: null, filter: {
+          titleId: 28
+        }) {
+          totalCount
+          pageInfo {
+            hasPreviousPage
+            hasNextPage
+            startCursor
+            endCursor
+          }
+          edges {
+            cursor
+            node {
               id
+              name
+              colorPrimary
+              colorSecondary
+              logoUrl
+              externalLinks {
+                dataProvider {
+                  name
+                }
+                externalEntity {
+                  id
+                }
+              }
             }
           }
         }
@@ -60,7 +74,7 @@ export class CSGOTeamsService {
             this.logger.error(`Unexpected response status: ${response.status}`);
             throw new Error(`Unexpected response status: ${response.status}`);
           }
-          return response.data.data.team;
+          return response.data.data.teams;
         }),
         catchError(err => {
           const status = err.response?.status;
@@ -74,9 +88,9 @@ export class CSGOTeamsService {
           } else if (status === 400) {
             throw new Error(`Bad Request: ${data.errors?.[0]?.message}`);
           } else if (status === 404) {
-            throw new Error('Not Found: The specified team does not exist');
+            throw new Error('Not Found: The specified resource does not exist');
           } else {
-            throw new Error('Error fetching team details');
+            throw new Error('Error fetching teams details');
           }
         })
       )
